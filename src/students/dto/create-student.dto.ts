bd18a96612107b6b473952486b791ece7729d65f
gustagonme <gustagonme@gmail.com>
2025-04-01 18:56:12 -0500

    handle error + create row

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,1 +1,22 @@
-export class CreateStudentDto {}
+import { IsArray, IsIn, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateStudentDto {
+
+    @IsString()
+    name:string;
+
+    @IsNumber()
+    @IsOptional()
+    @IsPositive()
+    age: number;
+
+    @IsString()
+    email:string;
+
+    @IsString({ each: true })
+    @IsArray()
+    subjects: string[];
+
+    @IsIn(['Male', 'Female', 'Other'])
+    gender: string;
+}
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+
+  private logger = new Logger('StudentService');
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>
+  ){}
+
+  async create(createStudentDto: CreateStudentDto) {
+    try{
+      const student = this.studentRepository.create(createStudentDto);
+      await this.studentRepository.save(student);
+      return student
+    }catch(error){
+      this.logger.error(error);
+      this.handleExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all students`;
+  async findAll() {
+    try{
+      const student = await this.studentRepository.find();
+      return student;
+    }catch(error){
+      throw new InternalServerErrorException("La volví a embarrar");
+    }
   }
 
   findOne(id: number) {
@@ -22,5 +44,13 @@ export class StudentsService {
 
   remove(id: number) {
     return `This action removes a #${id} student`;
+  }
+
+  private handleExceptions(error: any){
+    if(error.code === "23505")
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unspected error, check your server');
   }
 }
